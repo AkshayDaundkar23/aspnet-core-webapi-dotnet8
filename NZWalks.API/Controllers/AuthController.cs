4d6d94074fc8b07c47f7af24b6b8a795e6789ced
Akshay Daundkar <akshaydaundkar186@gmail.com>
2025-12-07 13:57:27 +0530

    Added Auth controller to create User.

---
+++ NZWalks.API/Controllers/AuthController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Models.DTO;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        public readonly UserManager<IdentityUser> _userManager;
+        public AuthController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        [Route("Register")]
+        public async Task<IActionResult> Register([FromBody] RegisterRequestDTO registerRequestDTO)
+        {
+            var identityUser = new IdentityUser
+            { 
+                UserName = registerRequestDTO.Username,
+                Email = registerRequestDTO.Username
+            };
+            var identityResult = await _userManager.CreateAsync(identityUser, registerRequestDTO.Password);
+
+            if (identityResult.Succeeded)
+            {
+                // add roles to user
+                if (registerRequestDTO.Roles != null && registerRequestDTO.Roles.Any())
+                {
+                    identityResult = await _userManager.AddToRolesAsync(identityUser, registerRequestDTO.Roles);
+
+                    if (identityResult.Succeeded)
+                    {
+                        return Ok("User registerd, Please login");
+                    }
+                }                
+            }
+
+            return BadRequest("Something went wrong");
+        }
+    }
+}
