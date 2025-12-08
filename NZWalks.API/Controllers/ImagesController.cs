e5d8d1c5b4344585c991fc7724891e362f821fda
Akshay Daundkar <akshaydaundkar186@gmail.com>
2025-12-08 18:28:52 +0530

    Added functionality to upload image to table.

---
+++ NZWalks.API/Controllers/ImagesController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Models.Domain;
+using NZWalks.API.Models.DTO;
+using NZWalks.API.Repositories;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImagesController : ControllerBase
+    {
+        private readonly IImageRepository _imageRepository;
+        public ImagesController(IImageRepository imageRepository)
+        {
+            _imageRepository = imageRepository;
+        }
+        // Post : /api/Images/UploadImage
+        [HttpPost]
+        [Route("UploadImage")]
+        public async Task<IActionResult> UploadImage([FromForm] ImageUploadRequestDTO imageUploadRequestDTO)
+        {
+            ValidateFileUpload(imageUploadRequestDTO);
+
+            if (ModelState.IsValid)
+            {
+                // convert DTO to domain
+
+                var imageDomainModel = new Image
+                {
+                    File = imageUploadRequestDTO.File,
+                    FileExtention = Path.GetExtension(imageUploadRequestDTO.File.FileName),
+                    FileSizeInBytes = imageUploadRequestDTO.File.Length,
+                    FileName = imageUploadRequestDTO.FileName,
+                    FileDescription = imageUploadRequestDTO.FileDescription,
+                };
+
+                await _imageRepository.UploadImage(imageDomainModel);
+
+                return Ok(imageDomainModel);
+            }
+
+            return BadRequest(ModelState);
+        }
+
+        // private method to validate file
+        private void ValidateFileUpload(ImageUploadRequestDTO imageUploadRequestDTO)
+        {
+            var allowedExtension = new string[] { ".jpg", ".jpeg", ".png" };
+
+            if (!allowedExtension.Contains(Path.GetExtension(imageUploadRequestDTO.File.FileName).ToLowerInvariant()))
+            {
+                ModelState.AddModelError("File", "Unsupported File Extenstion");
+            }
+
+            if (imageUploadRequestDTO.File.Length > 10485760)
+            {
+                ModelState.AddModelError("File", "File size more than 10MB, Please upload file with less than 10MB size");
+            }
+        }
+    }
+}
---
+++ NZWalks.API/Repositories/LocalImageRepository.cs
@@ -0,0 +1,40 @@
+﻿using NZWalks.API.Data;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Repositories
+{
+    public class LocalImageRepository : IImageRepository
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly NZWalksDbContext _dbContext;
+        public LocalImageRepository(IWebHostEnvironment webHostEnvironment, IHttpContextAccessor httpContextAccessor, NZWalksDbContext dbContext)
+        {
+            _webHostEnvironment = webHostEnvironment;
+            _httpContextAccessor = httpContextAccessor;
+            _dbContext = dbContext;
+        }
+        public async Task<Image> UploadImage(Image image)
+        {
+            var localFilePath = Path.Combine(_webHostEnvironment.ContentRootPath, "Images", $"{image.FileName}{image.FileExtention}");
+
+            // upload image to file path 
+            using var stream = new FileStream(localFilePath, FileMode.Create);
+            await image.File.CopyToAsync(stream);
+
+            // hpps://localhost:1234/images/image.jpg
+
+            var urlFilePath = $"{_httpContextAccessor.HttpContext.Request.Scheme}://" +
+                $"{_httpContextAccessor.HttpContext.Request.Host}{_httpContextAccessor.HttpContext.Request.PathBase}/Images/{image.FileName}{image.FileExtention}";
+
+            image.FilePath = urlFilePath;
+
+            // Add image to Images table
+
+            await _dbContext.Images.AddAsync(image);
+            await _dbContext.SaveChangesAsync();
+
+            return image;
+        }
+    }
+}
